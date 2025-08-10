0f80e09c462f9189e82f431d9174a518582b3750
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-08-10 23:01:09 +0300

    solve task

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,34 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    public final Map<String, UserData>  userDataMap = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (userDataMap.containsKey(userId)) {
+            return CompletableFuture.completedFuture(userDataMap.get(userId));
+        }
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return CompletableFuture.supplyAsync(
+                () -> new UserData(userId, "Details for" + userId), executor)
+                .thenApply(userData -> {
+                    userDataMap.put(userId, userData);
+                    return userData;
+                });
     }
 }
